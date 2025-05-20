aa02c033cbad358f4e83b95332f225f173fe402c
mztski-zhk <mztski@outlook.com>
2025-05-20 23:43:20 +0800

    added the tts feature.

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from langchain.chat_models import init_chat_model
 from langgraph.prebuilt import create_react_agent
+from pydantic import BaseModel, Field
+
 
 import src.stt
 
 # init
 import os
+
+import src.tts
 if not os.environ.get("OPENAI_API_KEY"):
     api_key = os.environ["OPENAI_API_KEY"] = "11f52000dfbf683a72c0f5743b5bcaa2cfbc3b4789f08b6be2412289e445fafa"
 
 base_url = "https://openrouter.ai/api/v1"
-model = "together:deepseek-ai/Deepseek-R1-Distill-Llama-70B-free"
+model = "together:meta-llama/Llama-3.3-70B-Instruct-Turbo-Free"
 temperature = 0.4
 
 basemodel = init_chat_model(
@@ -26,13 +30,6 @@ You are a helpful assistant.
 """
 )
 
-promptagents = create_react_agent(
-    model=basemodel,
-    tools=[],
-    prompt="""
-You are a helpful assistant.
-"""
-)
 
 if __name__ == "__main__":
     for i in range(4):
@@ -49,5 +46,16 @@ if __name__ == "__main__":
             )
 
             print(response)
+
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+
+        try:
+            src.tts.TextToSpeech(response)
+            os.system("ffplay -nodisp -autoexit -volume 1000 -i 0.wav")
+        except FileNotFoundError as e:
+            print(f"File not found: {str(e)}")
+        except PermissionError as e:
+            print(f"Permission error: {str(e)}")
         except Exception as e:
             print(f"An error occurred: {str(e)}")
